477ae328f9068b59fef549906da7f7dee4ef17a5
Faaris Mohamed <mohamed8876@students.cptc.edu>
2023-01-10 10:09:59 -0800

    Add project files.

---
+++ AlmostEmptyStarter/Controllers/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AlmostEmptyStarter.Controllers.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            /* This will set the title either in the controller 
+             * or the index.*/
+            ViewData["Title"] = "Home Page";
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+    }
+}
